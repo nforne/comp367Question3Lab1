601115e5347360b6606594a267fb60272dbb8b58
Martin Nforne <mnforne@my.centennialcollege.ca>
2026-02-07 22:34:58 -0500

    ASP.NET tutorials - comp367Question3Lab1

---
+++ comp367Question3Lab1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace comp367Question3Lab1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
